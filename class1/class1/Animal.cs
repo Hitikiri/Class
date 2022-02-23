9f7ab43502b01b9749f7d49f7fb5a103e099a4bc
bekiricli <82258691+Hitikiri@users.noreply.github.com>
2022-02-23 18:41:42 +0300

    Class

---
+++ class1/class1/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace class1
+{
+    public class Animal
+    {
+        public Animal()
+        {
+            weight = -1;
+            gender = -1;
+            name = "Not Defined";
+        }
+        public int weight;
+        public string name;
+        public int gender;
+
+        public string GetAnimalProperties()
+        {
+            return String.Format("Weight: {0} Name:{1} Gender:{2}",weight,name,gender);
+        }
+    }
+}
---
+++ class1/class1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace class1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Animal();
+            cat.name = "pisicik";
+            cat.weight = 4;
+            cat.gender = 1;
+            Console.WriteLine(cat.GetAnimalProperties());
+            
+            Animal bird = new Animal();
+            bird.name = "kanarya";
+            bird.weight = 1;
+            bird.gender = 0;
+            Console.WriteLine(bird.GetAnimalProperties());
+            
+            Animal dog = new Animal();
+            Console.WriteLine(dog.GetAnimalProperties());
+
+        }
+    }
+}
